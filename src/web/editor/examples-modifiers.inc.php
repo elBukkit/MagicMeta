8fd5e9d9b13e63111c88fca5e6ca3f1c3404e61b
Nathan Wolf <nathan@elmakers.com>
2020-11-10 22:35:41 -0800

    Modifiers example

---
+++ src/web/editor/examples-modifiers.inc.php
@@ -0,0 +1,15 @@
+<?php ?>
+
+<div id="defaultTemplates" style="display: none">
+    <textarea id="templateBlank" class="template" data-label="Blank Modifier"></textarea>
+    <textarea id="templateBasic" class="template" data-label="Basic Modifier"># This is the key name of this modifier
+# It must be unique across the server, and is used in configs to refer to this modifier
+# Most often this would be by using the Modifier action to add or remove a modifier.
+mymodifier:
+  # Apply some vanilla attributes
+  entity_attributes:
+    generic_knockback_resistance:
+      value: 2
+      operation: base
+    </textarea>
+</div>
