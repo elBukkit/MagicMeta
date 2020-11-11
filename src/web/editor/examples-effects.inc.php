e9ebc5365f0ec25ea136c2a5313a6604626e2f8a
Nathan Wolf <nathan@elmakers.com>
2020-11-10 22:38:29 -0800

    Add effects exammple

---
+++ src/web/editor/examples-effects.inc.php
@@ -0,0 +1,22 @@
+<?php ?>
+
+<div id="defaultTemplates" style="display: none">
+    <textarea id="templateBlank" class="template" data-label="Blank Effects"></textarea>
+    <textarea id="templateBasic" class="template" data-label="Fountain Effects"># This is the key name of this effects
+# It must be unique across the server, and is used in configs to refer to this effects
+# Most often this would be by using the PlayyEffects action, or in place of an effects list in
+# wand or spell configs.
+myfountain:
+ # Unlike most other configuration types in Magic, these are lists, not maps.
+ # Each item in the list is an individual effect player
+ - effectlib:
+     class: Fountain
+     duration: 86400000
+     particles_spout: 15
+     particles_strand: 20
+     particle_offset_x: 0.1
+     particle_offset_y: 0.1
+     particle_offset_z: 0.1
+     particle_count: 5
+    </textarea>
+</div>
--- src/web/editor/examples-modifiers.inc.php
+++ src/web/editor/examples-modifiers.inc.php
@@ -2,7 +2,7 @@
 
 <div id="defaultTemplates" style="display: none">
     <textarea id="templateBlank" class="template" data-label="Blank Modifier"></textarea>
-    <textarea id="templateBasic" class="template" data-label="Basic Modifier"># This is the key name of this modifier
+    <textarea id="templateBasic" class="template" data-label="Vanilla Attribute Modifier"># This is the key name of this modifier
 # It must be unique across the server, and is used in configs to refer to this modifier
 # Most often this would be by using the Modifier action to add or remove a modifier.
 mymodifier:
