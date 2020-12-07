2ff6703186ef5ff16ffbfbfdbd5ecb924dcc0c76
Nathan Wolf <nathan@elmakers.com>
2020-12-07 09:09:57 -0800

    Fix the editor breaking if you don't have a skin

--- src/web/editor/index.php
+++ src/web/editor/index.php
@@ -1,6 +1,6 @@
 <?php
 require_once('../config.inc.php');
-$version = 3;
+$version = 4;
 
 $session = null;
 $sessionId = null;
--- src/web/editor/js/main.js
+++ src/web/editor/js/main.js
@@ -64,10 +64,12 @@ function initialize() {
         if (_session.hasOwnProperty('player') && _session.player != null) {
             let user = _session.player;
             let skinUrl = user.skinUrl;
-            skinUrl = skinUrl.replace('http://', '//');
+            if (skinUrl) {
+                skinUrl = skinUrl.replace('http://', '//');
+                $('#userSkin').css('background-image', 'url("' + skinUrl + '")');
+                $('#userOverlay').css('background-image', 'url("' + skinUrl + '")');
+            }
             $('#userName').text(user.name);
-            $('#userSkin').css('background-image', 'url("' + skinUrl + '")');
-            $('#userOverlay').css('background-image', 'url("' + skinUrl + '")');
         }
     } else {
         editor.startNew("Basic");
