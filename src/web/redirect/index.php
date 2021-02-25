ffa16ee86779b087aeac5f87acc4514800dfc0fd
Nathan Wolf <nathan@elmakers.com>
2021-02-25 09:22:21 -0800

    Add some config aliases

--- src/web/redirect/index.php
+++ src/web/redirect/index.php
@@ -2,6 +2,8 @@
 $example = '';
 $subdomain = explode('.', $_SERVER['HTTP_HOST'])[0];
 if ($subdomain != 'survival' && $subdomain != 'mine' && $subdomain != 'magic') {
+    if ($subdomain == 'bend') $subdomain = 'bending';
+    if ($subdomain == 'hp') $subdomain = 'potter';
     $example = '?example=' . $subdomain;
 }
 header('Location: https://magic.elmakers.com/' . $example);
