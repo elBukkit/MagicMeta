3ef36ba4aa91e1043f8d157376b91d22170dca0a
Nathan Wolf <nathan@elmakers.com>
2021-03-04 15:46:37 -0800

    Add hollow world example

--- src/web/editor/examples-worlds.inc.php
+++ src/web/editor/examples-worlds.inc.php
@@ -18,4 +18,20 @@ world_nether_amplified:
     environment: nether
     # Load this world on server startup
     autoload: true</textarea>
+    <textarea id="templateHollow" class="template" data-label="Hollow World"># This is the key name of the world
+# It must be unique across the server, a world folder will be created with this name when the world is first
+# used, or if autoload is set to true.
+hollow:
+    # This is modifying the default overworld!
+    world: world
+    chunk_generate:
+        replace:
+          class: ReplacePopulator
+          replace:
+            stone: cave_air
+            dirt: cave_air
+            andesite: cave_air
+            gravel: cave_air
+            diorite: cave_air
+            granite: cave_air</textarea>
 </div>
