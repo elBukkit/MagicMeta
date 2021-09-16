6ea6bfdb78583cdcd95f9610d6ed6be391e8a3fb
Nathan Wolf <nathan@elmakers.com>
2021-09-15 23:27:58 -0700

    Map parameters and descriptions, collapse mini meta

--- bin/minmeta.php
+++ bin/minmeta.php
@@ -12,5 +12,21 @@ $inFile = $argv[1];
 $outFile = $argv[2];
 
 $in = json_decode(file_get_contents($inFile), true);
-$out = array('classed' => $in['classed']);
+$properties = $in['properties'];
+foreach ($in['classed'] as $classType => $classes) {
+    $mapped = array();
+    foreach ($classes as $key => $value) {
+        $parameters = $value['parameters'];
+        $mappedParameters = array();
+        foreach ($parameters as $parameter => $defaultVAlue) {
+            $property = $properties[$parameter];
+            // Note this is not a literal return
+            $description = implode($property['description'], '\n');
+            $mappedParameters[$property['field']] = $description;
+        }
+        $value['parameters'] = $mappedParameters;
+        $mapped[$key] = $value;
+    }
+    $out[$classType] = $mapped;
+}
 file_put_contents($outFile, json_encode($out));
