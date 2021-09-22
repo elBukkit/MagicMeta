7c107081a9f6de0fdb4a2583f1cd3df041177ff9
Nathan Wolf <nathan@elmakers.com>
2021-09-22 10:37:01 -0700

    Remove parameters from min meta since we are not using them for now.

--- bin/minmeta.php
+++ bin/minmeta.php
@@ -17,15 +17,7 @@ $properties = $in['properties'];
 foreach ($in['classed'] as $classType => $classes) {
     $mapped = array();
     foreach ($classes as $key => $value) {
-        $parameters = $value['parameters'];
-        $mappedParameters = array();
-        foreach ($parameters as $parameter => $defaultVAlue) {
-            $property = $properties[$parameter];
-            // Note this is not a literal return
-            $description = implode($property['description'], '\n');
-            $mappedParameters[$property['field']] = $description;
-        }
-        $value['parameters'] = $mappedParameters;
+        unset($value['parameters']);
         $mapped[$key] = $value;
     }
     $out[$classType] = $mapped;
